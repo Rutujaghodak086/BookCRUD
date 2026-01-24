9f73db3174052959b0708e869b2b83bf566d1614
Rutuja ghodake <ghodakerutuja086@gmail.com>
2026-01-24 12:12:33 +0530

    Add

---
+++ BookCRUD/src/main/java/com/example/demo/BookCrudApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import com.example.demo.model.Book;
+import com.example.demo.service.BookService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BookCrudApplication {
+
+    private final BookService bookService;
+
+
+    BookCrudApplication(BookService bookService) {
+        this.bookService = bookService;
+    }
+	
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCrudApplication.class, args);
+		
+		BookService bs=new BookService();
+		bs.br.save(new Book(111, "Java", "Rutuja", 500f));
+		
+	}
+
+}
---
+++ BookCRUD/src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,56 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+	
+	@Id
+	private Integer id;
+	private String name;
+	private String autherName;
+	private Float price;
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Book(Integer id, String name, String autherName, Float price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.autherName = autherName;
+		this.price = price;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAutherName() {
+		return autherName;
+	}
+	public void setAutherName(String autherName) {
+		this.autherName = autherName;
+	}
+	public Float getPrice() {
+		return price;
+	}
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", autherName=" + autherName + ", price=" + price + "]";
+	}
+	
+	
+
+}
